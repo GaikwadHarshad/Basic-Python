f8cc9f594e7c7aa4efba2414daf009393a0b06bb
mrgaikwad0605 <mr.gaikwad0605@gmail.com>
2019-03-06 12:34:17 +0530

    concatenatation of dictionaries

--- Utility/UtilityDS.py
+++ Utility/UtilityDS.py
@@ -9,3 +9,10 @@ def validate_num(num):
 def add_key_dictionary(d):
     d.update({3: 15})
     return d
+
+
+def concatenate_dict(d1, d2, d3):
+    d4 = {}
+    for d in (d1, d2, d3):
+        d4.update(d)
+    return d4
