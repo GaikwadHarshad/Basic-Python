cf799a5678813e59bcf6d4e9d1b8b6e7f4f8236b
mrgaikwad0605 <mr.gaikwad0605@gmail.com>
2019-03-14 18:51:54 +0530

    create a tuple with different data types

---
+++ WEEK 2/Tuple/program2.py
@@ -0,0 +1,17 @@
+"""  Write a Python program to create a tuple with different data types """
+
+
+class Tuple2:
+    try:
+        # creating tuple with different data types
+        tuple1 = ("python", 21.5, 6, 'p')
+
+        def tuple_diff_data(self):
+            print(self.tuple1)
+    except Exception as e:
+        print(e)
+
+
+# instance creation
+Tuple2_object = Tuple2()
+Tuple2_object.tuple_diff_data()
