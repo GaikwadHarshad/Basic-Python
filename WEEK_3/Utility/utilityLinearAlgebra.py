f3e554fc1b450df575b45f6552aaf3d7485468e5
mrgaikwad0605 <mr.gaikwad0605@gmail.com>
2019-03-15 14:30:44 +0530

    find inverse matrix of matrix X in problem 1

--- WEEK_3/Utility/utilityLinearAlgebra.py
+++ WEEK_3/Utility/utilityLinearAlgebra.py
@@ -52,4 +52,49 @@ class LinearAlgebra:
                 self.result[row][col] = matrix_1[row][col] * matrix_2[row][col]
         return self.result
 
+ # transpose of matrix
+    def transpose_matrix(self, matrix_1):
+        self.result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        for row in range(len(matrix_1)):
+            for col in range(len(matrix_1[0])):
+                self.result[col][row] = matrix_1[row][col]
+        return self.result
+
+    @staticmethod
+    def get_matrix_minor(m, i, j):
+        return [row[:j] + row[j + 1:] for row in (m[:i] + m[i + 1:])]
+
+    @staticmethod
+    def get_matrix_determinant(m):
+        # base case for 2x2 matrix
+        if len(m) == 2:
+            return m[0][0] * m[1][1] - m[0][1] * m[1][0]
+
+        determinants = (m[0][0] * (m[1][1] * m[2][2] - m[2][1] * m[1][2])
+                        - m[1][0] * (m[0][1] * m[2][2] - m[2][1] * m[0][2])
+                        + m[2][0] * (m[0][1] * m[1][2] - m[1][1] * m[0][2]))
+        return determinants
+
+    def get_inverse_matrix(self, m):
+        determinant = self.get_matrix_determinant(m)
+        # special case for 2x2 matrix:
+        if len(m) == 2:
+            return [[m[1][1] / determinant, -1 * m[0][1] / determinant],
+                    [-1 * m[1][0] / determinant, m[0][0] / determinant]]
+
+        # find matrix of co_factors
+        co_factors = []
+        for r in range(len(m)):
+            co_factor_row = []
+            for c in range(len(m)):
+                minor = self.get_matrix_minor(m, r, c)
+                co_factor_row.append(((-1) ** (r + c)) * self.get_matrix_determinant(minor))
+            co_factors.append(co_factor_row)
+        co_factors = self.transpose_matrix(co_factors)
+        for r in range(len(co_factors)):
+            for c in range(len(co_factors)):
+                co_factors[r][c] = co_factors[r][c] / determinant
+        return co_factors
+
+# ---------------------------------------------------------------------------------------------
 
