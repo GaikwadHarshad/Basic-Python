b6447f9767ddbf275b78ed49084ffd5add9f6710
mrgaikwad0605 <mr.gaikwad0605@gmail.com>
2019-03-22 14:44:41 +0530

    convert pandas module series to python list and its type

--- WEEK_4/Pandas/Programs/pandasProgram 1-10.py
+++ WEEK_4/Pandas/Programs/pandasProgram 1-10.py
@@ -13,6 +13,7 @@ class PandasProgram(UtilityClass):
     def pandas_operations(self):
         # list of programs
         print("1. create and display a 1-D array-like object containing an array of data using Pandas module")
+        print("2. convert a Panda module Series to Python list and it's type")
         print("0. Exit")
         print()
         while True:
@@ -25,14 +26,28 @@ class PandasProgram(UtilityClass):
                 if valid_choice:
                     choice = int(self.choice)
                     if choice == 1:
-                        num = int(input("How many element you want to add:"))
-                        if validate_num(num):
+                        n = int(input("How many element you want to add:"))
+                        if validate_num(n):
+                            num = int(n)
                             # 1D labeled array holding integer type data
                             data = UtilityClass.series_data(self, num)
                             print(data)
                         else:
                             print("Please enter numeric value")
-
+                    elif choice == 2:
+                        n = input("How many element you want to add:")
+                        if validate_num(n):
+                            num = int(n)
+                            # getting series of data from pandas module
+                            data = UtilityClass.series_data(self, num)
+                            print("Pandas Series : ""\n", data)
+                            # type of series
+                            print("Type of series : ", type(data))
+                            # convert series data into python list and display type
+                            print("Converted python list : ", data.tolist())
+                            print("Type of python list : ", type(data.tolist()))
+                        else:
+                            print("Please enter numeric value")
                     elif choice == 0:
                         exit()
                     else:
