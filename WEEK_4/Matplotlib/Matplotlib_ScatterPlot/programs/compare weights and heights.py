f542f6ca3b12941afb12c6bb1cc910e6b53ea5f6
mrgaikwad0605 <mr.gaikwad0605@gmail.com>
2019-03-25 17:01:02 +0530

    draw a scatter plot for three different groups comparing weights and heights.

--- WEEK_4/Matplotlib/Matplotlib_ScatterPlot/programs/compare weights and heights.py
+++ WEEK_4/Matplotlib/Matplotlib_ScatterPlot/programs/compare weights and heights.py
@@ -9,7 +9,7 @@ class CompareWeightsAndHeights:
 
     def scatter_plots(self):
         print()
-        print("1. Draw a scatter plot comparing two subject marks of Mathematics and Science.Use marks of 10 students.")
+        print("1.draw a scatter plot for three different groups comparing weights and heights. ")
         print("2. Exit")
         print()
         while True:
@@ -22,21 +22,15 @@ class CompareWeightsAndHeights:
                 if valid_choice:
                     choice = int(self.choice)
                     if choice == 1:
-                        print("Weight 1")
-                        weight1 = create_list_all(10)
-                        print("height 1:")
-                        height1 = create_list_all(10)
-                        print("Weight 2")
-                        weight2 = create_list_all(10)
-                        print("height 1:")
-                        height2 = create_list_all(10)
-                        print("Weight 3")
-                        weight3 = create_list_all(10)
-                        print("height 3:")
-                        height3 = create_list_all(10)
-                        weight = np.concatenate(weight1, weight2, weight3)
-                        height = np.concatenate(height1, height2, height3)
-                        plt.scatter(weight, height, marker='*', color=['red', 'green', 'blue'])
+                        weight1 = [67, 57.2, 59.6, 59.64, 55.8, 61.2, 60.45, 61, 56.23, 56]
+                        height1 = [101.7, 197.6, 98.3, 125.1, 113.7, 157.7, 136, 148.9, 125.3, 114.9]
+                        weight2 = [61.9, 64, 62.1, 64.2, 62.3, 65.4, 62.4, 61.4, 62.5, 63.6]
+                        height2 = [152.8, 155.3, 135.1, 125.2, 151.3, 135, 182.2, 195.9, 165.1, 125.1]
+                        weight3 = [68.2, 67.2, 68.4, 68.7, 71, 71.3, 70.8, 70, 71.1, 71.7]
+                        height3 = [165.8, 170.9, 192.8, 135.4, 161.4, 136.1, 167.1, 235.1, 181.1, 177.3]
+                        weight = np.concatenate((weight1, weight2, weight3))
+                        height = np.concatenate((height1, height2, height3))
+                        plt.scatter(weight, height, marker='*')
                         plt.xlabel("weight", fontsize=16)
                         plt.ylabel("height", fontsize=16)
                         plt.title("Group wise weight and height scatter plot", fontsize=20)
