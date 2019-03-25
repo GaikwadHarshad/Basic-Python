3d0306df29fafdebf9c32d55f972bbc0e55d7b09
mrgaikwad0605 <mr.gaikwad0605@gmail.com>
2019-03-25 10:13:39 +0530

    plot two or more lines with legends, different widths and colors

--- WEEK_4/Matplotlib/python matplotlib/Two or more line with diff style.py
+++ WEEK_4/Matplotlib/python matplotlib/Two or more line with diff style.py
@@ -1,11 +1,8 @@
-""" Write a Python program to plot two or more lines with legends, different widths and colors.  """
-
 import matplotlib.pyplot as plt
 from myprograms.WEEK_4.Matplotlib.Utility.UtilityModule import Utility
 
 
-class PlotTwoLines:
-
+class PlotTwoLineWithDiffStyle:
     # creates utility class object
     utility_obj = Utility()
 
@@ -14,37 +11,46 @@ class PlotTwoLines:
         # line 1 points
         x1 = int(input("how many values do u wanna insert in x-axis for line1"))
         x1_list = self.utility_obj.CreateList(x1)
-
         print(x1_list)
 
         y1 = int(input("how many values do u wanna insert in y-axis for line1"))
         y1_list = self.utility_obj.CreateList(y1)
         print(y1_list)
 
+        # plotting the line 1 points with dash line style
+        plt.plot(x1_list, y1_list, linestyle='dashed', label="line 1")
+
         # line 2 points
         x2 = int(input("how many values do u wanna insert in x-axis for line2"))
         x2_list = self.utility_obj.CreateList(x2)
-
         print(x2_list)
 
         y2 = int(input("how many values do u wanna insert in y-axis for line2"))
         y2_list = self.utility_obj.CreateList(y2)
         print(y2_list)
 
+        # plotting the line 2 points with Dotted line style
+        plt.plot(x2_list, y2_list, linestyle=':', label="line 2")
+
+        # line 3 points
+        x3 = int(input("how many values do u wanna insert in x-axis for line2"))
+        x3_list = self.utility_obj.CreateList(x3)
+        print(x3_list)
+
+        y3 = int(input("how many values do u wanna insert in y-axis for line2"))
+        y3_list = self.utility_obj.CreateList(y3)
+        print(y3_list)
+
+        # plotting the line 3 points with Dash-dot line style
+        plt.plot(x3_list, y3_list, linestyle='-.', label="line 2")
+
         # Set the x axis label
         plt.xlabel('x - axis')
-
         # Set the y axis label
         plt.ylabel('y - axis')
 
         # Sets a title
-        plt.title('Two or more lines on same plot with legends, different widths and colors ')
-
-        # plotting the line 1 points with color,width,label
-        plt.plot(x1_list, y1_list, color='blue', linewidth=2, label="line1-width-2")
-
-        # plotting the line 2 points with color,width,label
-        plt.plot(x2_list, y2_list, color='black', linewidth =4, label="line2-width-4")
+        plt.title('Two or more lines on same plot with suitable legends ')
 
         # show a legend on the plot
         plt.legend()
@@ -53,6 +59,5 @@ class PlotTwoLines:
         plt.show()
 
 
-# instantiation
-PlotTwoLines_obj = PlotTwoLines()
-PlotTwoLines_obj.draw_line()
+PlotTwoLineWithDiffStyle_obj = PlotTwoLineWithDiffStyle()
+PlotTwoLineWithDiffStyle_obj.draw_line()
