8df4c0fd6c90477b0d07a3caf92b3d7a7e459ede
mrgaikwad0605 <mr.gaikwad0605@gmail.com>
2019-05-04 19:32:00 +0530

    Build a machine learning model to predict user will click the ad or not based on his experience and estimated salary for a given dataset.

---
+++ WEEK_10/UtilityTemplate/UtilTemplate.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt  
+import pandas as pd
+from sklearn.preprocessing import Imputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import explained_variance_score
+from sklearn.metrics import *
+
+class Template:
+
+#     spliting dataset into train and test dataset
+    def split_datasets(self,dataset,size):
+        x_train,x_test = train_test_split(dataset,test_size=size)
+        return x_train,x_test
+    
+#     split data into train x,y and test x,y dataset
+    def split_data(self,x,y,test_size):
+        x_train,x_test,y_train,y_test = train_test_split(x,y,test_size)
+        return x_train,x_test,y_train,y_test
+    
+#     saving dataset into csv files
+    def save_csv(self,dataset, path):
+        return dataset.to_csv(path, index=False)
+
+#     reading csv files
+    def read_csv(self,path):
+        data = pd.read_csv(path)
+        return data
+    
+#     feature  scaling on x dataset
+    def feature_scaling(self,x):
+        ss = StandardScaler()
+        x = ss.fit_transform(x)
+        return x
+    
+#     fitting linear regression model
+    def Fit_Model(self,x,y):
+        regressor = LinearRegression()
+        regressor.fit(x,y)
+        return regressor
+    
+#     fitting logistic regression model
+    def Fit_Model_Logistic(self,x,y):
+        classifier = LogisticRegression()
+        classifier.fit(x,y)
+        return classifier
+    
+#    prediction over x data 
+    def prediction(self,x_data,regressor):
+        predicted = regressor.predict(x_data)
+        return predicted
